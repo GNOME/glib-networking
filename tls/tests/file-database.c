5507f0207a37a13c5b2efee66fb70030375ff0fd
Stef Walter <stefw@gnome.org>
2012-08-01 12:46:07 +0200

    gnutls: More tests for GTlsFileDatabaseGnutls
     * Check the properties
     * Check looking up certificates by issuer
    
    https://bugzilla.gnome.org/show_bug.cgi?id=681120

--- tls/tests/file-database.c
+++ tls/tests/file-database.c
@@ -291,8 +291,94 @@ test_file_database_handle_invalid (TestFileDatabase *test,
 }
 
 /* -----------------------------------------------------------------------------
- * BACKEND
+ * DATABASE
  */
+
+static void
+test_anchors_property (void)
+{
+  GTlsDatabase *database;
+  gchar *anchor_filename = NULL;
+  GError *error = NULL;
+
+  database = g_tls_file_database_new (TEST_FILE ("ca.pem"), &error);
+  g_assert_no_error (error);
+
+  g_object_get (database, "anchors", &anchor_filename, NULL);
+  g_assert_cmpstr (anchor_filename, ==, TEST_FILE ("ca.pem"));
+  g_free (anchor_filename);
+
+  g_object_unref (database);
+}
+
+static gboolean
+certificate_is_in_list (GList *certificates,
+                        const gchar *filename)
+{
+  GTlsCertificate *cert;
+  GError *error = NULL;
+  GList *l;
+
+  cert = g_tls_certificate_new_from_file (filename, &error);
+  g_assert_no_error (error);
+
+  for (l = certificates; l != NULL; l = g_list_next (l))
+    {
+      if (g_tls_certificate_equal (l->data, cert))
+        break;
+    }
+
+  g_object_unref (cert);
+
+  /* Had an early break from loop */
+  return l != NULL;
+}
+
+static void
+test_lookup_certificates_issued_by (void)
+{
+  /* This data is generated from the frob-certificate test tool in gcr library */
+  const guchar ISSUER[] = "\x30\x81\x86\x31\x13\x30\x11\x06\x0A\x09\x92\x26\x89\x93\xF2"
+                          "\x2C\x64\x01\x19\x16\x03\x43\x4F\x4D\x31\x17\x30\x15\x06\x0A"
+                          "\x09\x92\x26\x89\x93\xF2\x2C\x64\x01\x19\x16\x07\x45\x58\x41"
+                          "\x4D\x50\x4C\x45\x31\x1E\x30\x1C\x06\x03\x55\x04\x0B\x13\x15"
+                          "\x43\x65\x72\x74\x69\x66\x69\x63\x61\x74\x65\x20\x41\x75\x74"
+                          "\x68\x6F\x72\x69\x74\x79\x31\x17\x30\x15\x06\x03\x55\x04\x03"
+                          "\x13\x0E\x63\x61\x2E\x65\x78\x61\x6D\x70\x6C\x65\x2E\x63\x6F"
+                          "\x6D\x31\x1D\x30\x1B\x06\x09\x2A\x86\x48\x86\xF7\x0D\x01\x09"
+                          "\x01\x16\x0E\x63\x61\x40\x65\x78\x61\x6D\x70\x6C\x65\x2E\x63"
+                          "\x6F\x6D";
+
+  GList *certificates;
+  GByteArray *issuer_dn;
+  GTlsDatabase *database;
+  GError *error = NULL;
+
+  database = g_tls_file_database_new (TEST_FILE ("non-ca.pem"), &error);
+  g_assert_no_error (error);
+
+  issuer_dn = g_byte_array_new ();
+  /* The null terminator is in the array/string above */
+  g_byte_array_append (issuer_dn, ISSUER, G_N_ELEMENTS (ISSUER) - 1);
+
+  certificates = g_tls_database_lookup_certificates_issued_by (database, issuer_dn, NULL,
+                                                               G_TLS_DATABASE_LOOKUP_NONE,
+                                                               NULL, &error);
+
+  g_byte_array_unref (issuer_dn);
+
+  g_assert_cmpuint (g_list_length (certificates), ==, 4);
+
+  g_assert (certificate_is_in_list (certificates, TEST_FILE ("client.pem")));
+  g_assert (certificate_is_in_list (certificates, TEST_FILE ("client-future.pem")));
+  g_assert (certificate_is_in_list (certificates, TEST_FILE ("client-past.pem")));
+  g_assert (certificate_is_in_list (certificates, TEST_FILE ("server.pem")));
+  g_assert (!certificate_is_in_list (certificates, TEST_FILE ("server-self.pem")));
+
+  g_list_free_full (certificates, g_object_unref);
+  g_object_unref (database);
+  g_byte_array_unref (issuer_dn);
+}
 
 static void
 test_default_database_is_singleton (void)
@@ -340,6 +426,11 @@ main (int   argc,
               setup_verify, test_verify_database_bad_expired, teardown_verify);
   g_test_add ("/tls/database/verify-bad-combo", TestVerify, NULL,
               setup_verify, test_verify_database_bad_combo, teardown_verify);
+
+  g_test_add_func ("/tls/file-database/anchors-property",
+                   test_anchors_property);
+  g_test_add_func ("/tls/file-database/lookup-certificates-issued-by",
+                   test_lookup_certificates_issued_by);
 
   g_test_add ("/tls/file-database/test-handle", TestFileDatabase, NULL,
               setup_file_database, test_file_database_handle, teardown_file_database);
