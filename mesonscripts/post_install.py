5f159f81967cdf963bf42c73ffef2ac1317f9f03
Ignacio Casal Quinteiro <qignacio@amazon.com>
2017-10-24 09:49:37 +0200

    post_install: pass in the gio module dir
    This is to avoid having to run pkgconfig in a subprocess

--- mesonscripts/post_install.py
+++ mesonscripts/post_install.py
@@ -2,12 +2,10 @@
 
 import os
 import subprocess
+import sys
 
 # Packagers handle this
 if 'DESTDIR' not in os.environ:
-    # post install scripts can't pass these so we must check again
-    ret = subprocess.check_output(('pkg-config', '--variable',
-                                   'giomoduledir', 'gio-2.0'))
-    moduledir = ret.decode().strip()
+    moduledir = sys.argv[1]
     print('Updating module cache in {}...'.format(moduledir))
     subprocess.check_call(('gio-querymodules', moduledir))
