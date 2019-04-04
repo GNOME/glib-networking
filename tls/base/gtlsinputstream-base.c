9f14093fbed671aa92f275a95fa74fd5b76a185c
Michael Catanzaro <mcatanzaro@igalia.com>
2019-04-04 18:34:42 -0500

    base: update license headers and add modelines
    I switched glib-networking from LGPLv2.0+ to LGPLv2.1+ after
    glib-openssl was branched. The differences are minimal.
    
    Also, I like modelines.

--- tls/base/gtlsinputstream-base.c
+++ tls/base/gtlsinputstream-base.c
@@ -1,11 +1,13 @@
-/* GIO - GLib Input, Output and Streaming Library
+/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * GIO - GLib Input, Output and Streaming Library
  *
- * Copyright 2010 Red Hat, Inc.
+ * Copyright 2010 Red Hat, Inc
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
