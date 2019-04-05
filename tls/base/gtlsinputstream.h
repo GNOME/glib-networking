510ccdfa84f6188fc1f46d098fe672a40a48f95e
Michael Catanzaro <mcatanzaro@igalia.com>
2019-04-05 12:28:43 -0500

    base: Rename the streams to GTls[Input,Output]Stream
    Instead of GTls[Input,Output]StreamBase, since these classes are not
    inherited. This will be less confusing when the next commit turns them
    into final types.
    
    The naming is a bit iffy, since they are in the base/ directory after
    all, but I think it will be less confusing this way.

---
+++ tls/base/gtlsinputstream.h
@@ -0,0 +1,60 @@
+/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * GIO - GLib Input, Output and Streaming Library
+ *
+ * Copyright 2010 Red Hat, Inc
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * In addition, when the library is used with OpenSSL, a special
+ * exception applies. Refer to the LICENSE_EXCEPTION file for details.
+ */
+
+#ifndef __G_TLS_INPUT_STREAM_H__
+#define __G_TLS_INPUT_STREAM_H__
+
+#include <gio/gio.h>
+#include "gtlsconnection-base.h"
+
+G_BEGIN_DECLS
+
+#define G_TYPE_TLS_INPUT_STREAM            (g_tls_input_stream_get_type ())
+#define G_TLS_INPUT_STREAM(inst)           (G_TYPE_CHECK_INSTANCE_CAST ((inst), G_TYPE_TLS_INPUT_STREAM, GTlsInputStream))
+#define G_TLS_INPUT_STREAM_CLASS(class)    (G_TYPE_CHECK_CLASS_CAST ((class), G_TYPE_TLS_INPUT_STREAM, GTlsInputStreamClass))
+#define G_IS_TLS_INPUT_STREAM(inst)        (G_TYPE_CHECK_INSTANCE_TYPE ((inst), G_TYPE_TLS_INPUT_STREAM))
+#define G_IS_TLS_INPUT_STREAM_CLASS(class) (G_TYPE_CHECK_CLASS_TYPE ((class), G_TYPE_TLS_INPUT_STREAM))
+#define G_TLS_INPUT_STREAM_GET_CLASS(inst) (G_TYPE_INSTANCE_GET_CLASS ((inst), G_TYPE_TLS_INPUT_STREAM, GTlsInputStreamClass))
+
+typedef struct _GTlsInputStreamPrivate GTlsInputStreamPrivate;
+typedef struct _GTlsInputStreamClass   GTlsInputStreamClass;
+typedef struct _GTlsInputStream        GTlsInputStream;
+
+struct _GTlsInputStreamClass
+{
+  GInputStreamClass parent_class;
+};
+
+struct _GTlsInputStream
+{
+  GInputStream parent_instance;
+  GTlsInputStreamPrivate *priv;
+};
+
+GType         g_tls_input_stream_get_type (void) G_GNUC_CONST;
+GInputStream *g_tls_input_stream_new      (GTlsConnectionBase *conn);
+
+G_END_DECLS
+
+#endif /* __G_TLS_INPUT_STREAM_BASE_H___ */
