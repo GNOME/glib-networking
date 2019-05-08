1bdbd180655b5bbc0e0707fb415ba1b5b932c383
Michael Catanzaro <mcatanzaro@igalia.com>
2019-05-08 17:25:18 -0500

    Upgrade CI to Fedora 30
    We need access to glib 2.60

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -2,11 +2,17 @@
 
 set -e
 
-TAG="registry.gitlab.gnome.org/gnome/glib-networking/master:v2"
+TAG="registry.gitlab.gnome.org/gnome/glib-networking/master:v3"
 
 cd "$(dirname "$0")"
-docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
-    --file "Dockerfile" .
+
+# Workaround for error when running with sudo:
+#
+#Step 5/8 : RUN useradd -u $HOST_USER_ID -ms /bin/bash user
+# ---> Running in 2ac7b00c2788
+#useradd: UID 0 is not unique
+docker build --build-arg HOST_USER_ID=1000 --tag "${TAG}" --file "Dockerfile" .
+#docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" --file "Dockerfile" .
 
 if [ "$1" = "--push" ]; then
   docker login registry.gitlab.gnome.org
