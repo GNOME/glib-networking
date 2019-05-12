d5d82cf58ed8b9ad7071f67e0a0b675615e07178
Michael Catanzaro <mcatanzaro@igalia.com>
2019-05-12 16:48:37 -0500

    Remove hack from run-docker.sh
    This also bumps the version because I pushed a new image just for
    testing this commit, but we'll leave the CI on v3 since there shouldn't
    be any significant changes.

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -2,23 +2,17 @@
 
 set -e
 
-TAG="registry.gitlab.gnome.org/gnome/glib-networking/master:v3"
+TAG="registry.gitlab.gnome.org/gnome/glib-networking/master:v4"
 
 cd "$(dirname "$0")"
 
-# Workaround for error when running with sudo:
-#
-#Step 5/8 : RUN useradd -u $HOST_USER_ID -ms /bin/bash user
-# ---> Running in 2ac7b00c2788
-#useradd: UID 0 is not unique
-docker build --build-arg HOST_USER_ID=1000 --tag "${TAG}" --file "Dockerfile" .
-#docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" --file "Dockerfile" .
+sudo docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" --file "Dockerfile" .
 
 if [ "$1" = "--push" ]; then
-  docker login registry.gitlab.gnome.org
-  docker push $TAG
+  sudo docker login registry.gitlab.gnome.org
+  sudo docker push $TAG
 else
-  docker run --rm \
+  sudo docker run --rm \
       --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
       --tty --interactive "${TAG}" bash
 fi
