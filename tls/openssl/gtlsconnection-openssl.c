64622a97c114868807b4fe9c98f66f5459e8b351
Ignacio Casal Quinteiro <qignacio@amazon.com>
2019-05-14 16:31:49 +0200

    fixup! openssl: fix shutdown error

--- tls/openssl/gtlsconnection-openssl.c
+++ tls/openssl/gtlsconnection-openssl.c
@@ -105,9 +105,13 @@ end_openssl_io (GTlsConnectionOpenssl  *openssl,
     }
 
   /* This case is documented that it may happen and that is perfectly fine */
-  if (err_code == SSL_ERROR_SYSCALL && priv->shutting_down &&
-     (!my_error || g_error_matches(my_error, G_IO_ERROR, G_IO_ERROR_BROKEN_PIPE)))
-    return G_TLS_CONNECTION_BASE_OK;
+  if (err_code == SSL_ERROR_SYSCALL &&
+      ((priv->shutting_down && !my_error) ||
+       g_error_matches (my_error, G_IO_ERROR, G_IO_ERROR_BROKEN_PIPE)))
+    {
+      g_clear_error (&my_error);
+      return G_TLS_CONNECTION_BASE_OK;
+    }
 
   err = ERR_get_error ();
   err_lib = ERR_GET_LIB (err);
