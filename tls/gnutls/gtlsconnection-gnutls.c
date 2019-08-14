9b94ae442e6245ccf7372b9f8cecae5a831f133d
Michael Catanzaro <mcatanzaro@gnome.org>
2019-08-14 10:53:27 -0500

    gnutls: remove server support for session ID storage
    This doesn't work with TLS 1.3. Let's switch to session tickets instead,
    delegating storage to clients.

--- tls/gnutls/gtlsconnection-gnutls.c
+++ tls/gnutls/gtlsconnection-gnutls.c
@@ -483,7 +483,8 @@ end_gnutls_io (GTlsConnectionGnutls  *gnutls,
     status = end_gnutls_io (gnutls, direction, ret, err, errmsg);       \
   } while (status == G_TLS_CONNECTION_BASE_TRY_AGAIN);                  \
                                                                         \
-  if (status == G_TLS_CONNECTION_BASE_ERROR)                            \
+  if (status == G_TLS_CONNECTION_BASE_ERROR &&                          \
+      G_TLS_CONNECTION_GNUTLS_GET_CLASS (gnutls)-> failed)              \
     G_TLS_CONNECTION_GNUTLS_GET_CLASS (gnutls)->failed (gnutls);
 
 static void
