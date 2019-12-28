GNOME/glib-networking

commit 18360afb03b5b23887f6f027bddb4f19c9fc4be8

gnutls: Ensure gnutls_certificate_allocate_credentials() is successful
This should never happen, and it does never happen in my extensive
testing. But for robustness, we really should check to be sure it's not
happening.

tls/gnutls/gtlsconnection-gnutls.c
@@ -90,9 +90,7 @@ static void
 g_tls_connection_gnutls_init (GTlsConnectionGnutls *gnutls)
 {
   GTlsConnectionGnutlsPrivate *priv = g_tls_connection_gnutls_get_instance_private (gnutls);
-  gint unique_id;
-
-  gnutls_certificate_allocate_credentials (&priv->creds);
+  int unique_id;
 
   unique_id = g_atomic_int_add (&unique_interaction_id, 1);
   priv->interaction_id = g_strdup_printf ("gtls:%d", unique_id);
@@ -201,6 +199,7 @@ g_tls_connection_gnutls_initable_init (GInitable     *initable,
   gboolean client = G_IS_TLS_CLIENT_CONNECTION (gnutls);
   guint flags = client ? GNUTLS_CLIENT : GNUTLS_SERVER;
   int status;
+  int ret;
 
   g_object_get (gnutls,
                 "base-io-stream", &base_io_stream,
@@ -212,6 +211,10 @@ g_tls_connection_gnutls_initable_init (GInitable     *initable,
 
   if (base_socket)
     flags |= GNUTLS_DATAGRAM;
+
+  ret = gnutls_certificate_allocate_credentials (&priv->creds);
+  if (ret != GNUTLS_E_SUCCESS)
+    return FALSE;
 
   gnutls_init (&priv->session, flags);
 

tls/gnutls/gtlsclientconnection-gnutls.c
@@ -102,10 +102,6 @@ clear_gnutls_certificate_copy (GTlsClientConnectionGnutls *gnutls)
 static void
 g_tls_client_connection_gnutls_init (GTlsClientConnectionGnutls *gnutls)
 {
-  gnutls_certificate_credentials_t creds;
-
-  creds = g_tls_connection_gnutls_get_credentials (G_TLS_CONNECTION_GNUTLS (gnutls));
-  gnutls_certificate_set_retrieve_function2 (creds, g_tls_client_connection_gnutls_handshake_thread_retrieve_function);
 }
 
 static const gchar *
@@ -203,10 +199,13 @@ g_tls_client_connection_gnutls_initable_init (GInitable       *initable,
   GTlsConnectionGnutls *gnutls = G_TLS_CONNECTION_GNUTLS (initable);
   gnutls_session_t session;
   const gchar *hostname;
+  gnutls_certificate_credentials_t creds;
 
-  if (!g_tls_client_connection_gnutls_parent_initable_iface->
-      init (initable, cancellable, error))
+  if (!g_tls_client_connection_gnutls_parent_initable_iface->init (initable, cancellable, error))
     return FALSE;
+
+  creds = g_tls_connection_gnutls_get_credentials (G_TLS_CONNECTION_GNUTLS (gnutls));
+  gnutls_certificate_set_retrieve_function2 (creds, g_tls_client_connection_gnutls_handshake_thread_retrieve_function);
 
   session = g_tls_connection_gnutls_get_session (gnutls);
   hostname = get_server_identity (G_TLS_CLIENT_CONNECTION_GNUTLS (gnutls));

tls/gnutls/gtlsserverconnection-gnutls.c
@@ -96,10 +96,6 @@ clear_gnutls_certificate_copy (GTlsServerConnectionGnutls *gnutls)
 static void
 g_tls_server_connection_gnutls_init (GTlsServerConnectionGnutls *gnutls)
 {
-  gnutls_certificate_credentials_t creds;
-
-  creds = g_tls_connection_gnutls_get_credentials (G_TLS_CONNECTION_GNUTLS (gnutls));
-  gnutls_certificate_set_retrieve_function2 (creds, g_tls_server_connection_gnutls_handshake_thread_retrieve_function);
 }
 
 static void
@@ -120,10 +116,13 @@ g_tls_server_connection_gnutls_initable_init (GInitable       *initable,
   GTlsConnectionGnutls *gnutls = G_TLS_CONNECTION_GNUTLS (initable);
   GTlsCertificate *cert;
   gnutls_session_t session;
+  gnutls_certificate_credentials_t creds;
 
-  if (!g_tls_server_connection_gnutls_parent_initable_iface->
-      init (initable, cancellable, error))
+  if (!g_tls_server_connection_gnutls_parent_initable_iface->init (initable, cancellable, error))
     return FALSE;
+
+  creds = g_tls_connection_gnutls_get_credentials (G_TLS_CONNECTION_GNUTLS (gnutls));
+  gnutls_certificate_set_retrieve_function2 (creds, g_tls_server_connection_gnutls_handshake_thread_retrieve_function);
 
   session = g_tls_connection_gnutls_get_session (G_TLS_CONNECTION_GNUTLS (gnutls));
   gnutls_db_set_retrieve_function (session, g_tls_server_connection_gnutls_db_retrieve);

commit 629bbca0bf6f9ffe8ae5c7f3ec8255224c7f2d18

gnutls: ensure connection never fails without an error
This regressed in 18360afb03b5b23887f6f027bddb4f19c9fc4be8, when I
carelessly added a codepath that allows initialization to fail without
an error.

tls/gnutls/gtlsconnection-gnutls.c
@@ -137,7 +137,12 @@ g_tls_connection_gnutls_initable_init (GInitable     *initable,
 
   ret = gnutls_certificate_allocate_credentials (&priv->creds);
   if (ret != GNUTLS_E_SUCCESS)
-    return FALSE;
+    {
+      g_set_error (error, G_TLS_ERROR, G_TLS_ERROR_MISC,
+                   _("Could not create TLS connection: %s"),
+                   gnutls_strerror (ret));
+      return FALSE;
+    }
 
   gnutls_init (&priv->session, flags);
 