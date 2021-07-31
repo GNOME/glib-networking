cafc2bd935decb5dbd6333e7881297fdd23f44db
Patrick Griffis <pgriffis@igalia.com>
2021-07-31 13:13:24 -0500

    Regenerate certificates

--- tls/tests/certificate.h
+++ tls/tests/certificate.h
@@ -1,4 +1,4 @@
 /* This file is generated from update-certificate-test.py */
 
-#define EXPECTED_NOT_VALID_BEFORE "2021-06-23T20:16:15Z"
-#define EXPECTED_NOT_VALID_AFTER "2046-06-17T20:16:15Z"
+#define EXPECTED_NOT_VALID_BEFORE "2021-07-31T18:13:10Z"
+#define EXPECTED_NOT_VALID_AFTER "2046-07-25T18:13:10Z"
