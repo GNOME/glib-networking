611f3f78e8b79fe5de978a20a73df984ab21ed87
Michael Catanzaro <mcatanzaro@redhat.com>
2022-02-11 10:29:15 -0600

    proxy: update copyright year in environment proxy files

--- proxy/environment/genvironmentproxyresolver.h
+++ proxy/environment/genvironmentproxyresolver.h
@@ -2,7 +2,7 @@
 /*
  * GIO - GLib Input, Output and Streaming Library
  *
- * Copyright 2021 Red Hat Inc.
+ * Copyright © 2022 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
